1ff427c4fb1a78b4b176b3323bc2bd509402fb3d
Janusz Sołtysiak <jsoltysiak@gmail.com>
2015-09-14 21:50:51 +0200

    Added shake screen on explosion

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,29 @@
 var BoomRoomBox = BoomRoomBox || {};
 
-BoomRoomBox.Game = function () {};
+BoomRoomBox.Game = function () {
+    //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+
+    this.game;      //  a reference to the currently running game
+    this.add;       //  used to add sprites, text, groups, etc
+    this.camera;    //  a reference to the game camera
+    this.cache;     //  the game cache
+    this.input;     //  the global input manager (you can access this.input.keyboard, this.input.mouse, as well from it)
+    this.load;      //  for preloading assets
+    this.math;      //  lots of useful common math operations
+    this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc
+    this.stage;     //  the game stage
+    this.time;      //  the clock
+    this.tweens;    //  the tween manager
+    this.state;     //  the state manager
+    this.world;     //  the game world
+    this.particles; //  the particle manager
+    this.physics;   //  the physics manager
+    this.rnd;       //  the repeatable random number generator
+
+    //  You can use any of these from any function within this State.
+    //  But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
+
+};
 
 BoomRoomBox.Game.prototype = {
 
@@ -42,6 +65,11 @@ BoomRoomBox.Game.prototype = {
         }
 
         this.handleInput();
+        this.shakeScreen();
+    },
+
+    render: function() {
+        this.game.debug.body(this.player);
     },
 
     handleInput: function () {
@@ -158,6 +186,7 @@ BoomRoomBox.Game.prototype = {
     onBulletHitEnemy: function (enemy, bullet) {
         enemy.kill();
         bullet.kill();
+        this.shakeWorld = 5;
     },
 
     onBulletHitWall: function (bullet, wall) {
@@ -166,5 +195,17 @@ BoomRoomBox.Game.prototype = {
         var explosionAnimation = this.explosions.getFirstExists(false);
         explosionAnimation.reset(bullet.x, bullet.y);
         explosionAnimation.play('explosion', 200, false, true);
+    },
+
+    shakeScreen: function() {
+        if (this.shakeWorld > 0) {
+            var rand1 = this.game.rnd.integerInRange(-1, 1);
+            var rand2 = this.game.rnd.integerInRange(-1, 1);
+            this.game.world.setBounds(rand1, rand2, this.game.width + rand1, this.game.height + rand2);
+            this.shakeWorld--;
+            if (this.shakeWorld == 0) {
+                this.game.world.setBounds(0, 0, this.game.width, this.game.height); // normalize after shake?
+            }
+        }
     }
 };
